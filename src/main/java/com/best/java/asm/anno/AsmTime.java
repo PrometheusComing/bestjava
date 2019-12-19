78d4dd3cd40d1dd632c1a2925cb04eb74704559e
PrometheusComing <1226553206@qq.com>
2019-12-19 16:31:29 +0800

    first

---
+++ src/main/java/com/best/java/asm/anno/AsmTime.java
@@ -0,0 +1,16 @@
+package com.best.java.asm.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author: xjxu3
+ * @Date: 2019/12/12 9:55
+ * @Description:
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface AsmTime {
+}
