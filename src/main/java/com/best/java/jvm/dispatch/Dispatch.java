6964741bdace2ed4ef7f2d6dc32071277774442d
PrometheusComing <1226553206@qq.com>
2020-01-16 17:16:38 +0800

    综合分析，静态多分派，动态单分派

--- src/main/java/com/best/java/jvm/dispatch/Dispatch.java
+++ src/main/java/com/best/java/jvm/dispatch/Dispatch.java
@@ -3,7 +3,14 @@ package com.best.java.jvm.dispatch;
 /**
  * @Author: xjxu3
  * @Date: 2020/1/13 16:46
- * @Description:
+ * @Description: 综合考虑
+ * 宗量：方法调用者和方法参数，都叫宗量
+ * 先是编译期间的静态分派，此时编译器将根据静态类型和方法参数进行选择，结果两条指令（invokevirtual）分别选择了Father.hardChoice(_360)
+ * 和Father.hardChoice(QQ)，将符号引用分别给两条指令（invokevirtual）作为参数。方法调用者和参数都影响选择，所以属于静态多分派
+ *
+ * 然后是运行期间，动态分派的过程，在执行invokevirtual指令时（比如是son.hardChoice(new QQ());）,编译期间已经确定
+ * 目标方法签名必须是hardChoice(QQ),在当前实际类型Son中，找到hardChoice(QQ arg) ,直接调用后结束。只有方法调用者有影响，所以
+ * 属于动态单分派
  */
 
 public class Dispatch {
--- src/main/java/com/best/java/jvm/dispatch/DynamicDispatch.java
+++ src/main/java/com/best/java/jvm/dispatch/DynamicDispatch.java
@@ -4,6 +4,15 @@ package com.best.java.jvm.dispatch;
  * @Author: xjxu3
  * @Date: 2020/1/16 13:59
  * @Description: 动态分派：典型应用就是方法重写
+ * 方法调用者的确定要使用实际类型，即man和woman，虽然两条invokevirtual指令的参数都是
+ * com/best/java/jvm/dispatch/DynamicDispatch$Human.sayHello:()V，但实际最终执行
+ * 的目标方法是不同的，原因在于invokevirtual的多态查找过程：
+ * 1.找到操作数栈栈顶元素指向的对象的实际类型，如main方法里，man的实际类型是Man，静态类型是Human
+ * 2.如果在实际类型Man里找到常量池中描述符和简单名称都相符的方法(注意，不是在Man常量池里找，是Man的方法里)，则进行访问权限校验，校验通过则查找完成，返回该方法直接引用
+ * 3.否则按照继承关系从下往上依次对其父类进行第二步
+ *
+ * 这种运行期间根据实际类型确定方法调用版本的过程就是动态分派
+ * java只有动态单分派但是有静态多分派和静态单分派，JDK 7 及之前
  */
 public class DynamicDispatch {
 	static abstract class Human {
@@ -23,6 +32,7 @@ public class DynamicDispatch {
 			System.out.println("woman say hello");
 		}
 	}
+	//DynamicDispatch字节码
 //  public static void main(java.lang.String[]);
 //    descriptor: ([Ljava/lang/String;)V
 //    flags: ACC_PUBLIC, ACC_STATIC
@@ -47,14 +57,6 @@ public class DynamicDispatch {
 //        32: aload_1
 //        33: invokevirtual #6                  // Method com/best/java/jvm/dispatch/DynamicDispatch$Human.sayHello:()V
 //        36: return
-//      LineNumberTable:
-//        line 28: 0
-//        line 29: 8
-//        line 30: 16
-//        line 31: 20
-//        line 32: 24
-//        line 33: 32
-//        line 34: 36
 //      LocalVariableTable:
 //        Start  Length  Slot  Name   Signature
 //            0      37     0  args   [Ljava/lang/String;
@@ -69,3 +71,5 @@ public class DynamicDispatch {
 		man.sayHello();
 	}
 }
+
+
