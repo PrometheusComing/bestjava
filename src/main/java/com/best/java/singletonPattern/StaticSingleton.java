a05216f7f2438b9778a9f64fd2f6c7455ef367ea
PrometheusComing <1226553206@qq.com>
2020-01-21 14:27:29 +0800

    使用unsafe类进行反射

--- src/main/java/com/best/java/singletonPattern/StaticSingleton.java
+++ src/main/java/com/best/java/singletonPattern/StaticSingleton.java
@@ -1,5 +1,9 @@
 package com.best.java.singletonPattern;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 public class StaticSingleton {
 	private StaticSingleton () {
 		System.out.println("StaticSingleton is create");
@@ -7,7 +11,21 @@ public class StaticSingleton {
 	private static class SingletonHolder {
 		private static StaticSingleton instance = new StaticSingleton();
 	}
-	public StaticSingleton getInstance () {
+	public static StaticSingleton getInstance () {
 		return SingletonHolder.instance;
+	}
+
+	public static void main(String[] args) {
+		//使用unsafe破坏单例
+		StaticSingleton s1 = StaticSingleton.getInstance();
+		try {
+			Field field = Unsafe.class.getDeclaredField("theUnsafe");
+			field.setAccessible(true);
+			Unsafe unsafe = (Unsafe) field.get(null);
+			StaticSingleton s2 = (StaticSingleton)unsafe.allocateInstance(StaticSingleton.class);
+			System.out.println(s1 == s2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
