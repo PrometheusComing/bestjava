d8fc90cf596e2f59b71cfa83b5f3a771301793e6
PrometheusComing <1226553206@qq.com>
2020-01-27 13:27:16 +0800

    BIO,NIO,AIO补充2

--- src/main/java/com/best/java/io/bio/ClientSocket.java
+++ src/main/java/com/best/java/io/bio/ClientSocket.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
  *  优秀博文推荐地址：
  *  https://www.cnblogs.com/crazymakercircle/p/10225159.html
  *
- * 四大IO模型：BIO模型，NIO模型，IO多路复用，AIO。JDK NIO是结合了NIO + IO多路复用
+ * 四大IO模型：BIO模型，NIO模型（没有使用select或者epoll系统调用，read和write在无数据时不阻塞，有数据时，阻塞等待内核缓冲区
+ * 数据复制到用户缓冲区），IO多路复用，AIO。JDK NIO是结合了NIO + IO多路复用（使用select或epoll系统调用）
  *
  *  JDK BIO（同步阻塞IO）：使用一个独立的Acceptor线程来监听客户端的连接（一个连接就是一个线程）
  *  弊端：每个客户端请求都需要一个线程去处理，无法满足高性能、高并发场景
