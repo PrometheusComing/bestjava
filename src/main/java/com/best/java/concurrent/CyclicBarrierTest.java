1e06ef535070f1feaf6f3f6d012c84d5b22dc561
PrometheusComing <1226553206@qq.com>
2020-03-09 21:01:19 +0800

    补充注释

--- src/main/java/com/best/java/concurrent/CyclicBarrierTest.java
+++ src/main/java/com/best/java/concurrent/CyclicBarrierTest.java
@@ -20,6 +20,8 @@ public class CyclicBarrierTest implements Runnable {
 		try {
 			Thread.sleep(1000);
 			System.out.println(Thread.currentThread().getName() + " 到达栅栏 A");
+			// 使用await对计数进行加1，计数不到初始化容量的时候，所有线程互相等待，当到达容量时，由最后一个线程执行最后的任务后，其他线程继续执行。且
+			// 执行后计数清零
 			cyclicBarrier.await();
 			System.out.println(Thread.currentThread().getName() + " 冲破栅栏 A");
 
