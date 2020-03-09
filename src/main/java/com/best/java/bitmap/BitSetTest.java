27d1709be47219650d6775dd59b3e79c00ea8c75
PrometheusComing <1226553206@qq.com>
2020-03-09 12:05:46 +0800

    bitsettest

---
+++ src/main/java/com/best/java/bitmap/BitSetTest.java
@@ -0,0 +1,26 @@
+package com.best.java.bitmap;
+
+import java.util.BitSet;
+
+/**
+ * @Author: xjxu3
+ * @Date: 2020/3/9 11:34
+ * @Description:  bitmap的java实现
+ */
+public class BitSetTest {
+	public static void main(String[] args) {
+		BitSet bitSet = new BitSet();
+		bitSet.set(0);
+		System.out.println(bitSet.size());
+		bitSet.set(1);
+		System.out.println(bitSet.size());
+		bitSet.set(70);
+		System.out.println(bitSet.size());
+		System.out.println(bitSet.get(0));
+		System.out.println(bitSet.get(1));
+		System.out.println(bitSet.get(70));
+		System.out.println(bitSet.get(90));
+		bitSet = new BitSet(90);
+		System.out.println(bitSet.size());
+	}
+}
