7ab04fcb08189508aef637ef3828d08e7d752202
PrometheusComing <1226553206@qq.com>
2020-03-10 23:05:56 +0800

    hashmap补充

--- src/main/java/com/best/java/hashmap/HashMapTest.java
+++ src/main/java/com/best/java/hashmap/HashMapTest.java
@@ -2,6 +2,7 @@ package com.best.java.hashmap;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: xjxu3
@@ -21,6 +22,7 @@ public class HashMapTest {
 	}
 
 	public static void main(String[] args) {
+//		test 1
 //		HashMap<String, String> map = new HashMap<>(2,1.0f);
 //		map.put("Aa","Aa");
 //		map.put("BB","BB");
@@ -41,27 +43,43 @@ public class HashMapTest {
 //			e.printStackTrace();
 //		}
 
-		HashMap<HashMapTest, String> map = new HashMap<>(10);
-		for (int i =0;i < 1;i ++ ) {
-			map.put(new HashMapTest(),"123");
-		}
 
 
-		Class cls = map.getClass();
+//		test 2
+//		HashMap<HashMapTest, String> map = new HashMap<>(10);
+//		for (int i =0;i < 1;i ++ ) {
+//			map.put(new HashMapTest(),"123");
+//		}
+//
+//
+//		Class cls = map.getClass();
+//
+//		try {
+//			Field field = cls.getDeclaredField("table");
+//			field.setAccessible(true);
+//			try {
+//				Object[] objects = (Object[]) field.get(map);
+//				System.out.println(objects.length);
+//			} catch (IllegalAccessException e) {
+//				e.printStackTrace();
+//			}
+//		} catch (NoSuchFieldException e) {
+//			e.printStackTrace();
+//		}
 
-		try {
-			Field field = cls.getDeclaredField("table");
-			field.setAccessible(true);
-			try {
-				Object[] objects = (Object[]) field.get(map);
-				System.out.println(objects.length);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		}
 
-
+		Map<String, String> map = new HashMap<>();
+		map.put("1", null);
+		System.out.println(map.get("1"));
+//		map.putIfAbsent("1", null);
+		map.put("1", "1");
+		System.out.println(map.get("1"));
+		map.putIfAbsent("1", "2");
+		System.out.println(map.get("1"));
+		map.put("1", null);
+		System.out.println(map.get("1"));
+		// 不会覆盖旧值，但是旧值是null，依旧会覆盖
+		map.putIfAbsent("1", "2");
+		System.out.println(map.get("1"));
 	}
 }
