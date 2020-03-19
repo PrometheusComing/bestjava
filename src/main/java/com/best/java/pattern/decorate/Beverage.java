724e56e7e5634ba20dba5c823065cfce67b3ca58
PrometheusComing <1226553206@qq.com>
2020-03-19 09:21:25 +0800

    策略

--- src/main/java/com/best/java/pattern/decorate/Beverage.java
+++ src/main/java/com/best/java/pattern/decorate/Beverage.java
@@ -5,6 +5,8 @@ package com.best.java.pattern.decorate;
  *
  * 定义饮料基类，饮料的描述，饮料的价格（需要具体实现才知道）
  * 饮料的描述，或者说味道，和价格，就是装饰功能，独立成子类
+ *
+ * ps:可以进一步把原本价格作为私有域，价格计算行为封装出去作为策略。来应对商品满减和打折。即和策略模式一起使用
  */
 public abstract class Beverage {
 	private String description = "Unknown Beverage";
