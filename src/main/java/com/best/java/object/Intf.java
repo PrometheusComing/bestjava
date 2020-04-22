e7f586fe67ec418ef1bcfb48c2ad059420684c11
PrometheusComing <1226553206@qq.com>
2020-04-22 10:07:50 +0800

    接口的变量默认编译为常量

--- src/main/java/com/best/java/object/Intf.java
+++ src/main/java/com/best/java/object/Intf.java
@@ -3,8 +3,9 @@ package com.best.java.object;
 /**
  * @Author: xjxu3
  * @Date: 2019/12/2 17:23
- * @Description:
+ * @Description: 接口里的变量默认编译为常量，即public static final
  */
 public interface Intf {
+	int a = 0;
 	void hello();
 }
