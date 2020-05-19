8b9c023977f3ec2dc22430975b44a1d7d72195e0
PrometheusComing <1226553206@qq.com>
2020-05-19 15:04:29 +0800

    命令模式更新

--- src/main/java/com/best/java/pattern/command/Julia.java
+++ src/main/java/com/best/java/pattern/command/Julia.java
@@ -58,15 +58,4 @@ public class Julia {
 		keyPadImprove.execute("stopCommand");
 		keyPadImprove.execute("marco");
 	}
-
-//	public static void main(String[] args) {
-//		//创建接收者对象
-//		AudioPlayer audioPlayer = new AudioPlayer();
-//		//创建命令对象
-//		Command playCommand = new PlayCommand(audioPlayer);
-//		Command rewindCommand = new RewindCommand(audioPlayer);
-//		Command stopCommand = new StopCommand(audioPlayer);
-//
-
-//	}
 }
